318c6be77a559ea920aeb3e071c4cd9705fc6ddc
surender <surenderpal134@gmail.com>
2020-04-01 15:51:03 +0530

    added Grammerly

--- office_open_applicaton.py
+++ office_open_applicaton.py
@@ -4,3 +4,5 @@ webbrowser.open('https://ads-release-3-10-np.groundtruth.com/login?')
 webbrowser.open('https://stackoverflow.com/users/login?')
 webbrowser.open('https://id.atlassian.com/login?')
 webbrowser.open('https://groundtruth.okta.com/')
+webbrowser.open('https://www.grammarly.com/signin?')
+
--- self_learning_application.py
+++ self_learning_application.py
@@ -3,3 +3,4 @@ import webbrowser
 webbrowser.open('https://www.udemy.com/course/learn-complete-python-tutorial-in-simple-way/')
 webbrowser.open('https://github.com/login')
 webbrowser.open('https://stackoverflow.com/users/login?')
+webbrowser.open('https://www.grammarly.com/signin?')
